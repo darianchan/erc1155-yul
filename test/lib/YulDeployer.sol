5c456f41482d9be008195df1686f0db4ce7a6d45
darianchan <darian.s.chan@gmail.com>
2023-07-20 13:57:26 -0700

    initial setup to get tests working

--- test/lib/YulDeployer.sol
+++ test/lib/YulDeployer.sol
@@ -9,7 +9,7 @@ contract YulDeployer is Test {
     ///@param fileName - The file name of the Yul contract. For example, the file name for "Example.yul" is "Example"
     ///@return deployedAddress - The address that the contract was deployed to
     function deployContract(string memory fileName) public returns (address) {
-        string memory bashCommand = string.concat('cast abi-encode "f(bytes)" $(solc --yul yul/', string.concat(fileName, ".yul --bin | tail -1)"));
+        string memory bashCommand = string.concat('cast abi-encode "f(bytes)" $(solc --strict-assembly yul/', string.concat(fileName, ".yul --bin | tail -1)"));
 
         string[] memory inputs = new string[](3);
         inputs[0] = "bash";
@@ -17,6 +17,7 @@ contract YulDeployer is Test {
         inputs[2] = bashCommand;
 
         bytes memory bytecode = abi.decode(vm.ffi(inputs), (bytes));
+        // bytes memory bytecode = hex"600a600d600039600a6000f3fe600035608052366080f3";
 
         ///@notice deploy the bytecode with the create instruction
         address deployedAddress;
